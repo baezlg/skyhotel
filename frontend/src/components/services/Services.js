5a0031faf3b2dda4ce6975ad2e95f99a5c614bec
basil george <baezl4life@gmail.com>
2022-01-17 16:23:42 +0200

    creating services component

---
+++ frontend/src/components/services/Services.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "./Services.scss";
+
+const Services = () => {
+  return (
+    <section className="services">
+      <h1 className="heading">facilities & services</h1>
+      <div className="services__container">
+        <div className="services__box">
+          <div className="services__icon fas fa-bed"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">Luxury Rooms</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-wifi"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">Fast & Free Wifi</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-phone-alt"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">Call Us 24/7</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-credit-card"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">Accepts Credit Card</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-utensils"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">Restaurant</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-dumbbell"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">World class gym</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-taxi"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">taxi</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+        <div className="services__box">
+          <div className="services__icon fas fa-calendar-check"></div>
+          <div className="services__info">
+            <h4 className="services__info-heading">event hosting</h4>
+            <div className="services__info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              ut quo rerum rem dicta ipsam ullam cumque maxime similique vero!
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Services from "../../components/services/Services";
 import Featured from "./../../components/featured/Featured";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <Services />
     </div>
   );
 };
